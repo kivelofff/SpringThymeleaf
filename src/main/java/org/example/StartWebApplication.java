78c716b80ffb5ea458cdfde4176807dd045d668b
ladmin <kivelofff@gmail.com>
2020-09-16 02:03:07 +0200

    Initial commit

---
+++ src/main/java/org/example/StartWebApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartWebApplication.class, args);
+    }
+}
